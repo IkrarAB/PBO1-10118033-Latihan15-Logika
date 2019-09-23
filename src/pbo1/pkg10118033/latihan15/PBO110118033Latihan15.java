d7dd5b6e7794dec4c32a2d37a0c371f3fcdc9233
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 23:15:20 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan15/PBO110118033Latihan15.java
@@ -0,0 +1,22 @@
+package pbo1.pkg10118033.latihan15;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan15 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        
+        System.out.println("a && b = " + (a&&b));
+        System.out.println("a || b = " + (a||b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a&&b));
+    }
+}
